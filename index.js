f6340a54443d60438c9dff8107574d697272c3ec
Teewai57 <bobtoyoabasi@gmail.com>
2022-07-22 20:48:04 +0100

    object class

---
+++ index.js
@@ -0,0 +1,64 @@
+// let user = new Object(); //object constructor
+
+// let user = {}; // object literal syntax
+
+
+// let contact = [
+//     "John", "Doe"
+// ]
+// console.log((typeof contact));
+// delete user.age
+
+
+// delete user.name
+// console.log(user.name);
+// console.log(user);
+
+
+
+
+
+
+
+// let user = {
+//     name: "Toyo",
+//     age: 50,
+//     class: "Js4",
+//     gender: "Female"
+// };
+//   for (let key in user) {
+//     alert( key );
+//     alert ( user[key] )
+//   }
+
+
+
+
+
+
+
+// alert(user)
+
+// let user = {
+//     name: "Toyo",
+//     age: 21,
+// }
+
+// console.log(Object.values(user));
+
+
+
+
+
+
+
+
+// function Users( name, age ) {
+//     this.name = name;
+//     this.age = age;
+// }
+
+// let user1 = new Users( "Toyo", 21 ) ;
+
+// console.log(user1);
+
---
+++ main.js
@@ -0,0 +1,43 @@
+let user = {
+    name: "Abbas",
+    age: 36,
+    gender: "Female"
+}
+
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.log(Object.entries(user));
+
+// delete (user.gender)
+
+// user.gender = "Female"
+// user.food = "Rice"
+
+// console.log(user);
+
+
+// for (key in user ) {
+//     // console.log(key);
+//     console.log(user[key]);
+// }
+
+
+
+// console.log(Object.values(user));
+// delete user.name
+// console.log(user.name);
+// console.log(user);
+
+
+
+// function Users (name, age) {
+//     this.name = name;
+//     this.age = age;
+// }
+
+// let users = new Users( "Mary", 32 );
+
+// console.log(users);
+
+
+
